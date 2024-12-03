41fe5dddcb25f2dfd8fc42244de3ee98371efeb8
g-abilio <gabrielabiliobferreira2@gmail.com>
2024-12-03 01:01:15 -0300

    fix(run script): restrict compatibility temporarily

--- run.sh
+++ run.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    pip install -r ./requirements.txt
-    python video_detection.py
-
-elif [[ "$(uname -s)" == "Darwin" ]]; then
-    python3 -m pip install -r ./requirements.txt
-    python3 video_detection.py
-
-elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-    pip install -r .\requirements.txt
-    python video_detection.py
-
-else
-    echo "Unsupported operating system."
-    exit 1
-fi
+python3 -m pip install -r ./requirements.txt
+python3 video_detection.py
